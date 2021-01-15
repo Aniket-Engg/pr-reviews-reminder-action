62717dda0343d6b25a3ed81f2fb200206780d164
yann300 <yann.levreau@gmail.com>
2021-01-15 23:02:21 +0100

    onlyGitter

--- functions.js
+++ functions.js
@@ -2,43 +2,14 @@ function getPullRequestsWithRequestedReviewers(pullRequests) {
   return pullRequests.filter(pr => pr.requested_reviewers.length);
 }
 
-function createPr2UserArray(pullRequestsWithRequestedReview) {
-  const pr2user = [];
-  for (const pr of pullRequestsWithRequestedReview) {
-    for (const user of pr.requested_reviewers) {
-      pr2user.push({ url: pr.html_url, login: user.login });
-    }
-  }
-  return pr2user;
-}
-
-// Convert string like "davideviolante:ID123,foobar:ID456" to 
-// object like { davideviolante: "ID123", foobar: "ID456" }
-function stringToObject(str) {
-  const map = {};
-  if (!str) {
-    return map;
-  }
-  const users = (str || '').split(',');
-  users.forEach(user => {
-    const [github, provider] = user.split(':');
-    map[github] = provider
-  });
-  return map;
-}
-
-function prettyMessage(pr2user, github2provider, provider) {
-  let message = '';
-  for (const obj of pr2user) {
-    const mention = github2provider[obj.login] ? `<@${github2provider[obj.login]}>` : `@${obj.login}`;
-    switch (provider) {
-      case 'slack':
-        message += `Hey ${mention}, this PR is waiting for your review: ${obj.url}\n`;
-        break;
-      case 'msteams':
-        message += `Hey ${mention}, this PR is waiting for your review: [${obj.url}](${obj.url})  \n`;
-        break;
-    }
+function prettyMessage(prs) {
+  let message = 'Hey guys, just a tiny reminder about PRs that need review\n';
+  for (const pr of prs) {
+      message += `[${pr.html_url}](${pr.html_url}) - `
+      for (const user of pr.requested_reviewers) {
+          message += ` @${user.login}`
+      }
+      message += '\n'
   }
   return message;
 }
@@ -46,6 +17,5 @@ function prettyMessage(pr2user, github2provider, provider) {
 module.exports = {
   getPullRequestsWithRequestedReviewers,
   createPr2UserArray,
-  stringToObject,
   prettyMessage
 };
--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const axios = require('axios');
 
 const {
   getPullRequestsWithRequestedReviewers,
-  createPr2UserArray,
   prettyMessage,
-  stringToObject
 } = require("./functions");
 
 const GITHUB_API_URL = 'https://api.github.com';
@@ -24,35 +22,24 @@ function getPullRequests() {
   });
 }
 
-function sendNotification(webhookUrl, channel, message) {
-  return axios({
-    method: 'POST',
-    url: webhookUrl,
-    data: {      
-      text: message,
-    },
-    headers: {
+function sendNotification(webhookUrl, message) {
+  console.log(message)
+  return axios.post(webhookUrl, { text : message }, {headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': `Bearer ${GITTER_TOKEN}`
-    }
-  });
+    }})
 }
 
 async function main() {
   try {
-    const webhookUrl = core.getInput('webhook-url');
-    const provider = core.getInput('provider');
-    const channel = core.getInput('channel');
-    const github2providerString = core.getInput('github-provider-map');
+    const webhookUrl = core.getInput('webhook-url');   
     core.info('Getting open pull requests...');
     const pullRequests = await getPullRequests();
     core.info(`There are ${pullRequests.data.length} open pull requests`);
     const pullRequestsWithRequestedReviewers = getPullRequestsWithRequestedReviewers(pullRequests.data);
     core.info(`There are ${pullRequestsWithRequestedReviewers.length} pull requests waiting for reviews`);
     if (pullRequestsWithRequestedReviewers.length) {
-      const pr2user = createPr2UserArray(pullRequestsWithRequestedReviewers);
-      const github2provider = stringToObject(github2providerString);
       const message = prettyMessage(pr2user, github2provider, provider);
       await sendNotification(webhookUrl, channel, message);
       core.info(`Notification sent successfully!`);

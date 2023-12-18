051dfd8169f3f43ac4fdfc556a090a2441cf1a58
aniket-engg <aniketengg.225@gmail.com>
2023-12-18 13:09:52 +0530

    limit

--- index.js
+++ index.js
@@ -43,29 +43,30 @@ function sendEmbeds(webhookUrl, embeds) {
 async function doRepo(pulls_endpoint, webhookUrl, title) {
   core.info('doRepo--pulls_endpoint->', pulls_endpoint)
   const pullRequests = await getPullRequests(pulls_endpoint);
-  await sendNotification(webhookUrl, `Hey guys! There are ${pullRequests.data.length} open pull requests`);
   const prs = getPullRequestsWithRequestedReviewers(pullRequests.data);
   core.info(`There are ${prs.length} pull requests waiting for reviews`);
+  await sendNotification(webhookUrl, `There are ${prs.length} pull requests waiting for reviews under ${title} repo`);
   if (prs.length) {
     console.log(prs)
     let embeds = []
-    for (const pr of prs) {
+    for (let i=0; i < 10; i++) {
+      const pr = prs[i]
       let embed = {}
-      let reviewers = null
+      let reviewers = ''
       for (const user of pr.requested_reviewers)
         reviewers += ` @${user.login}`
       
       embed.title = pr.title
       embed.url = pr.html_url
-      // embed.fields = [{
-      //   name: "Review required by:",
-      //   value: reviewers
-      // }]
+      console.log('reviewers----->', reviewers)
+      embed.fields = [{
+        name: "Review required by:",
+        value: reviewers
+      }]
       console.log('embed--->', embed)
       embeds.push(embed)
-      await sendEmbeds(webhookUrl, embeds);
-
     }
+    await sendEmbeds(webhookUrl, embeds);
     core.info(`Notification sent successfully!`);
   }
 }
@@ -74,7 +75,7 @@ async function main() {
   try {
     core.info('inside main');
     const webhookUrl = core.getInput('webhook-url');   
-    // await sendNotification(webhookUrl, 'Hey guys! just a tiny reminder about PRs that need review')
+    await sendNotification(webhookUrl, 'Hello Remix Team, a gentle request to review pending PRs')
     core.info('Getting open pull requests...');
     await doRepo(PULLS_ENDPOINT, webhookUrl, 'remix-project')
     await doRepo(`${GITHUB_API_URL}/repos/ethereum/remix-plugins-directory/pulls`, webhookUrl, 'remix-plugins-directory')
